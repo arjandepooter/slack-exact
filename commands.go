1ab10d9366cc0872bb366484369b8fbe40f821bf
Arjan de Pooter <arjan@cmsworks.nl>
2015-07-07 22:12:54 +0200

    Fixed schema

--- commands.go
+++ commands.go
@@ -6,19 +6,19 @@ import (
 
 // SlackCommand represents the payload of the Slack slash commands
 type SlackCommand struct {
-	token       string `schema:"token"`
-	teamID      string `schema:"team_id"`
-	teamDomain  string `schema:"team_domain"`
-	channelID   string `schema:"channel_id"`
-	channelName string `schema:"channel_name"`
-	userID      string `schema:"user_id"`
-	userName    string `schema:"user_name"`
-	command     string `schema:"command"`
-	text        string `schema:"text"`
+	Token       string `schema:"token"`
+	TeamID      string `schema:"team_id"`
+	TeamDomain  string `schema:"team_domain"`
+	ChannelID   string `schema:"channel_id"`
+	ChannelName string `schema:"channel_name"`
+	UserID      string `schema:"user_id"`
+	UserName    string `schema:"user_name"`
+	Command     string `schema:"command"`
+	Text        string `schema:"text"`
 }
 
 func (command *SlackCommand) getParsedText() []string {
-	parsedText, err := shellwords.NewParser().Parse(command.text)
+	parsedText, err := shellwords.NewParser().Parse(command.Text)
 	if err != nil {
 		return []string{}
 	}
