e64c51e0c46dd4117236ee1892e863e686d45019
Arjan de Pooter <arjan@cmsworks.nl>
2015-07-07 22:11:09 +0200

    Defined some more settings

--- config.go
+++ config.go
@@ -5,11 +5,26 @@ import (
 	"log"
 )
 
-type Config struct {
+type configSpecification struct {
+	// Port
 	Port int `default:"3000"`
+	// URL of the Slack Webhook
+	WebhookURL string `envconfig:"webhook_url"`
+	// Token for incoming Slack requests
+	IncomingToken string `envconfig:"incoming_token"`
+	// Username for bot replies
+	BotUsername string `envconfig:"bot_username" default:"Exact Online"`
+	// Base URL of Slack Exact
+	BaseURL string `envconfig:"base_url" default:"http://slackexact.greymug.io/"`
+	// Exact app key
+	AppKey string `envconfig:"app_key"`
+	// Exact client ID
+	ClientID string `envconfig:"client_id"`
+	// Exact client secret
+	ClientSecret string `envconfig:"client_secret"`
 }
 
-var config Config
+var config configSpecification
 
 func init() {
 	err := envconfig.Process("slack_exact", &config)
